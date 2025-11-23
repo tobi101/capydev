bc1757856e5953f3395f83bc3eba0b7c6b3cf42b
Sergeev Sergey <Sergeev_sergey99@mail.ru>
2025-11-23 20:18:56 +0300

    карточки

--- src/services.js
+++ src/services.js
@@ -12,15 +12,23 @@ export function renderServices(servicesData, lang) {
     servicesData.items.forEach(service => {
         const serviceCard = document.createElement('div');
         serviceCard.className = 'service-card card';
+        
+        // Устанавливаем фоновое изображение через CSS переменную
+        if (service.backImage) {
+            serviceCard.style.setProperty('--back-image', `url('${service.backImage}')`);
+        }
 
         const featuresHTML = service.features.map(feature => 
             `<li>${feature[lang]}</li>`
         ).join('');
 
         serviceCard.innerHTML = `
-            <span class="service-icon">${service.icon}</span>
-            <h3 class="service-title">${service.title[lang]}</h3>
-            <p class="service-description">${service.description[lang]}</p>
+            ${service.backImage ? `<div class="service-back-image" style="background-image: url('${service.backImage}')"></div>` : ''}
+            <div class="service-content">
+                <span class="service-icon">${service.icon}</span>
+                <h3 class="service-title">${service.title[lang]}</h3>
+                <p class="service-description">${service.description[lang]}</p>
+            </div>
             <ul class="service-features">
                 ${featuresHTML}
             </ul>
